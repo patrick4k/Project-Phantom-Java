90b419c0e5746958f7ff8a1755745095febd4949
patrick4k <pmkennedy4@gmail.com>
2022-04-29 12:28:53 -0400

    Add files via upload

--- Moon.java
+++ Moon.java
@@ -43,15 +43,19 @@ public class Moon extends Grain implements Serializable {
     }
 
     @Override
+    // TODO Fix inhibited ends area (maybe wait for port area to be complete?)
         public void calcBurnArea(double regTotal) {
         double burnArea = 0;
         double Do = getOuterDiameter();
         double l = getGrainLength() - (2-getInhibitedEnds())*regTotal;
         double d = innerDiameter + 2*regTotal;
-        if (0 <= ((Do-d)/2)-offset) {
+        if ((Do/2 <= d/2 - offset) || ((2-getInhibitedEnds())*regTotal > getGrainLength())) {
+            burnArea = 0;
+        }
+        else if (Do/2 >= d/2 + offset) {
             burnArea = Math.PI*d*l;
         }
-        else if (0 <= ((Do-d)/2)+offset) {
+        else if (Do/2 >= d/2 - offset) {
             double yBar = (.5/offset)*(0.25*(Math.pow(Do,2)-Math.pow(d,2))+Math.pow(offset,2));
             double gamma = 0;
             if (yBar >= 0) {
@@ -69,38 +73,27 @@ public class Moon extends Grain implements Serializable {
                 burnArea = ((Math.PI/2)-theta)*d*l + (2-getInhibitedEnds())*gamma*Math.pow(d,2)/8;
             }
         }
+        burnArea += (2-getInhibitedEnds())*((Math.PI/4)*Math.pow(Do,2)-getPortArea());
         setBurnArea(burnArea);
     }
 
     @Override
-    // TODO Something is wrong here // def need to fix
     public void calcPortArea(double regTotal) {
         double portArea = 0;
         double Do = getOuterDiameter();
+        double l = offset;
         double d = innerDiameter + 2*regTotal;
-        if (0 <= ((Do-d)/2)-offset) {
+
+        if (Do/2 >= d/2 + l) {
             portArea = (Math.PI/4)*(Math.pow(d,2));
         }
-        else if (0 <= ((Do-d)/2)+offset) {
-            double yBar = (.5/offset)*(0.25*(Math.pow(Do,2)-Math.pow(d,2))+Math.pow(offset,2));
-            double gamma;
-
-            if (yBar >= 0) {
-                gamma = Math.PI + 2*Math.asin(2*yBar/Do);
-            }
-            else {
-                gamma = Math.PI - 2 * Math.asin(-2 * yBar / Do);
-            }
-            if (gamma > 2*Math.PI) {
-                gamma = 2*Math.PI;
-            }
-            else if (gamma < 0) {
-                gamma = 0;
-            }
-            System.out.println(gamma/Math.PI);
-            portArea = (2*Math.PI - gamma)*Math.pow(d,2)/8;
+        else if (Do/2 >= d/2 - l) {
+            double x = Math.sqrt(-(1/(64*l*l))*(d*d*d*d - 2*Do*Do*d*d - 8*l*l*d*d + Do*Do*Do*Do + 16*l*l*l*l - 8*Do*Do*l*l)); // x value of intersection
+            portArea = (Do*Do/8)*(Math.asin(2*x/Do)+(0.5)*Math.sin(2*Math.asin(2*x/Do))) - 2*l*x + (d*d/4)*(Math.asin(2*x/d) + (0.5)*Math.sin(2*Math.asin(2*x/d)));
         }
-        //System.out.println(portArea);
+        else {
+            portArea = (Math.PI/4)*(Math.pow(Do,2));
+        }
         setPortArea(portArea);
     }
 
